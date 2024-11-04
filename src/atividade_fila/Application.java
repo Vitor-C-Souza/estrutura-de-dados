351f6ba045b170a99df529aa07f6239b8a4aeab0
Vitor-C-Souza <vitor_souza97@hotmail.com>
2024-11-04 12:04:31 -0300

    Finalizado a lista de exercicios

--- src/atividade_fila/Application.java
+++ src/atividade_fila/Application.java
@@ -1,4 +1,66 @@
 package atividade_fila;
 
+import java.util.LinkedList;
+import java.util.Scanner;
+
 public class Application {
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+
+        LinkedList<String> fila = new LinkedList<>();
+
+        while (true) {
+            System.out.println("""
+                    ********************************************************************
+                    
+                            1 - Adicionar Clientes na Fila
+                            2 - Listar todos os clientes
+                            3 - Retirar Clientes da Fila
+                            0 - Sair
+                    
+                    ********************************************************************
+                    Entre com a opção desejada:
+                    """);
+            int opcao = read.nextInt();
+            read.skip("\\R?");
+
+            switch (opcao) {
+                case 1:
+                    System.out.println("Digite o nome:");
+                    fila.add(read.nextLine());
+
+                    System.out.println("Fila: \n");
+                    for (String pessoa : fila) {
+                        System.out.println(pessoa);
+                    }
+                    System.out.println("Cliente Adicionado!\n");
+                    break;
+                case 2:
+                    System.out.println("Lista de Clientes na Fila: \n");
+                    for (String pessoa : fila) {
+                        System.out.println(pessoa);
+                    }
+                    break;
+                case 3:
+                    fila.remove();
+                    if (fila.isEmpty()) {
+                        System.out.println("A Fila está vazia!");
+                    } else {
+                        System.out.println("Fila: \n");
+                        for (String pessoa : fila) {
+                            System.out.println(pessoa);
+                        }
+                    }
+                    break;
+                case 0:
+                    System.out.println("Programa Finalizado!");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Opção invalida!!");
+                    break;
+            }
+        }
+
+    }
 }
--- src/atividade_pilha/Application.java
+++ src/atividade_pilha/Application.java
@@ -1,4 +1,65 @@
 package atividade_pilha;
 
+import java.util.Scanner;
+import java.util.Stack;
+
 public class Application {
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+
+        Stack<String> pilha = new Stack<>();
+
+        while (true) {
+            System.out.println("""
+                    ********************************************************************
+                    
+                            1 - Adicionar Louça suja
+                            2 - Mostrar a Louça suja
+                            3 - Retirar Louça suja
+                            0 - Sair
+                    
+                    ********************************************************************
+                    Entre com a opção desejada:
+                    """);
+            int opcao = read.nextInt();
+            read.skip("\\R?");
+            switch (opcao) {
+                case 1:
+                    System.out.println("Digite o nome:");
+                    pilha.push(read.nextLine());
+
+                    System.out.println("Pilha: ");
+                    for (String louca : pilha) {
+                        System.out.println(louca);
+                    }
+
+                    System.out.println("Louça adicionada!");
+                    break;
+                case 2:
+                    System.out.println("Mostrando a Louça suja: ");
+                    for (String louca : pilha) {
+                        System.out.println(louca);
+                    }
+                    break;
+                case 3:
+                    if (pilha.isEmpty()) {
+                        System.out.println("A louça suja foi lavada ou nunca existiu!!!");
+                    } else {
+                        pilha.pop();
+
+                        System.out.println("Pilha: ");
+                        for (String louca : pilha) {
+                            System.out.println(louca);
+                        }
+                    }
+                case 0:
+                    System.out.println("Programa Finalizado!");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Opção invalida!!");
+                    break;
+            }
+        }
+    }
 }
